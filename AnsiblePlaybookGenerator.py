5f3cbd8cc691b9671a979b5beb7990bfb8b614ff
Harley Bates <Harley.Bates@kpmg.co.uk>
2020-04-05 11:32:32 +0100

    Writing Ansible Config

--- AnsiblePlaybookGenerator.py
+++ AnsiblePlaybookGenerator.py
@@ -2,6 +2,19 @@ import shutil
 import os
 
 PATH = '/tmp/ansible_files'
+CONFIG_TEMPLATE ="""[default]
+timeout=10
+private_key_file = ~/.ssh/id_rsa
+host_key_checking = False
+[privilege_escalation]
+become = yes
+become_method = sudo
+become_user = root
+become_ask_path = False
+[ssh_connection]
+scp_if_true = True
+timeout = 10"""
+
 
 def CleanUp(path):
     shutil.rmtree(path+'/tasks')
@@ -9,4 +22,14 @@ def CleanUp(path):
     shutil.rmtree(path+'/group_vars')
     os.remove(path+'/run.yml')
 
+
+def AnsibleCfg(path, config):
+    if os.path.isfile(path+'ansible.cfg'):
+        print("Config Exists")
+    else:
+        with open(f'{path}/ansible.cfg', 'w') as ansibleconfig:
+            ansibleconfig.write(config)
+
+
 CleanUp(PATH)
+AnsibleCfg(PATH, CONFIG_TEMPLATE)
