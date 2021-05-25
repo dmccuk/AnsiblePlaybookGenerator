c835d719ba052e11a32e54aa4d2ff4292543fbf1
Cloud User <ec2-user@ip-172-31-16-55.eu-west-2.compute.internal>
2021-05-25 13:49:49 +0000

    updates

--- AnsiblePlaybookGenerator.sh
+++ AnsiblePlaybookGenerator.sh
@@ -157,27 +157,7 @@ cat << EOF >> $RUNDIR/run.yml
 EOF
 
 cat << EOF >> $RUNDIR/templates/$template.j2
-<!doctype html>
-<title>LondonIAC meetup - Site Maintenance</title>
-<style>
-  body { text-align: center; padding: 150px; }
-  h1 { font-size: 50px; }
-  body { font: 20px Helvetica, sans-serif; color: #333; }
-  article { display: block; text-align: left; width: 650px; margin: 0 auto; }
-  a { color: #dc8100; text-decoration: none; }
-  a:hover { color: #333; text-decoration: none; }
-</style>
-
-<article>
-    <h1>Your Server name is: {{ ansible_hostname }}</h1>
-    <div>
-        <p>Sorry for the inconvenience but we&rsquo;re performing some maintenance at the moment. If you need to you can always <a href="mailto:#">contact us</a>, otherwise we&rsquo;ll be back online shortly!</p>
-<img src="https://marcelorjava.files.wordpress.com/2014/04/dilbert.gif" alt="Dilbert">
-        <p>&mdash; The Team</p>
-    </div>
-    <p>
-</p>
-</article>
+Your Server name is: {{ ansible_hostname }}
 EOF
 fi
 done < keyFile
